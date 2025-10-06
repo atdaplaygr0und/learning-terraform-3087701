1cf68773a242158b1c85d82726f1dd08d0ca3941
atdaplaygr0und <124084754+atdaplaygr0und@users.noreply.github.com>
2025-10-06 12:44:44 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,22 @@
+variable "aws_region" {
+  description = "AWS region to deploy into"
+  type        = string
+  default     = "us-east-1"
+}
+
 variable "instance_type" {
-  description = "Type of EC2 instance to provision"
-  default     = "t3.nano"
+  description = "Free Tier–eligible type: t2.micro, t3.micro, or t4g.micro"
+  type        = string
+  default     = "t3.micro"
+
+  validation {
+    condition     = can(regex("^(t2|t3|t4g)\\.micro$", var.instance_type))
+    error_message = "Use t2.micro, t3.micro, or t4g.micro (all Free Tier–eligible in supported regions)."
+  }
+}
+
+variable "name" {
+  description = "Name tag for the instance"
+  type        = string
+  default     = "free-tier-ec2"
 }
